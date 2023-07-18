d8897d04957048ba3c382fa852b6d09cff958e84
Brayan-Alexis <brayanzxd@gmail.com>
2023-07-18 00:24:27 -0700

    01-arrow-functions

---
+++ src/es6/01-arrow-functions.js
@@ -0,0 +1,12 @@
+// ES5
+function square(num){
+    return num * num;
+}
+
+// ES6
+const squareArrow = (num) => {
+    return num * num; // Explicit return
+}
+
+// ES6 - Shorter
+const squareArrow2 = num => num * num; // Implicit return
