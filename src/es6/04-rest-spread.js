24881150b0788a32e866bb12a03c28e7903000bb
Brayan-Alexis <brayanzxd@gmail.com>
2023-07-27 18:25:59 -0700

    04-rest-spread spread operator

--- src/es6/04-rest-spread.js
+++ src/es6/04-rest-spread.js
@@ -13,3 +13,21 @@ let person = {
 let {name, age} = person;
 console.log(name, person.age); // John 30
 console.log(name, age); // John 30
+
+// Spread operator
+let user = { name: 'Charly', age: 27 };
+let country = 'USA';
+
+let data = { ...user, country }; //... is the spread operator
+let data2 = { id:1, ...user, country };
+console.log(data); // { name: 'Charly', age: 27, country: 'USA' }
+console.log(data2); // { id:1, name: 'Charly', age: 27, country: 'USA' }
+
+// Rest
+function sum(num, ...values) {
+    console.log(values); // [1, 2, 3, 4]
+    console.log(num + values[0]); // 2
+    return num + values[0];
+}
+
+sum(1, 1, 2, 3, 4);
