4fc0dda73825fa760ca180b2f1b24f2784a556be
Brayan-Alexis <brayanzxd@gmail.com>
2023-07-31 22:17:27 -0700

    10-set-add

---
+++ src/es6/10-set-add.js
@@ -0,0 +1,5 @@
+const list = new Set(); // Create new set
+list.add('Item 1'); // Add single item
+list.add('Item 2').add('Item 3'); // Add multiple items
+
+console.log(list); // Set { 'Item 1', 'Item 2', 'Item 3' }
