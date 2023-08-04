6c4c51adda9f11880bacb90e51061872f1ce4fe4
Brayan-Alexis <brayanzxd@gmail.com>
2023-08-03 20:11:44 -0700

    04-trailling-commas

---
+++ src/es8/04-trailling-commas.js
@@ -0,0 +1,4 @@
+const age = [23, 37, 18, 22, 45, 67, , , , 38, , 21, ,  ]; // trailing commas
+
+console.log(age); // [ 23, 37, 18, 22, 45, 67, <3 empty items>, 38, <1 empty item>, 21, <1 empty item> ]
+console.log(age.length); // 13
