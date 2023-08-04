e8008e3487e244909554438db8e9a0ede187f66e
Brayan-Alexis <brayanzxd@gmail.com>
2023-08-03 19:25:54 -0700

    01-object-keys-&-values

---
+++ src/es8/01-object-keys-&-values.js
@@ -0,0 +1,7 @@
+// Objects keys and values
+const countries = { MX: 'Mexico', CO: 'Colombia', AR: 'Argentina', PE: 'Peru', EC: 'Ecuador' };
+
+// Object.keys()
+console.log(Object.keys(countries)); // ['MX', 'CO', 'AR', 'PE', 'EC']
+// Object.values()
+console.log(Object.values(countries)); // ['Mexico', 'Colombia', 'Argentina', 'Peru', 'Ecuador']
