b4133279338d1f34403dfc6f23ebee032d8edf89
Brayan-Alexis <brayanzxd@gmail.com>
2023-08-06 05:06:51 -0700

    01-bigint

---
+++ src/es11/01-bigint.js
@@ -0,0 +1,5 @@
+const aBigNumber = 9007199254740991n; // n at the end to indicate that it is a BigInt
+const anotherBigNumber = BigInt(9007199254740991); // Another way to create a BigInt
+
+console.log(aBigNumber); // 9007199254740991n
+console.log(anotherBigNumber); // 9007199254740991n
